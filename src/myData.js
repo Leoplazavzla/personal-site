858d2df206b0a6bf6f4aafe429731086caa8562b
leoplazavzla <Caracter$$123>
2022-03-24 01:58:50 -0300

    Adapted the template to my needs and uploaded my information and projects

---
+++ src/myData.js
@@ -0,0 +1,172 @@
+// Skills Icons
+import cssIcon from "./images/css.svg"
+import jsIcon from "./images/javascript.svg"
+import codeIcon from "./images/code.svg"
+
+// Social Icon
+import githubIcon from "./images/github.svg"
+import codepenIcon from "./images/codepen.svg"
+import dribbbleIcon from "./images/dribbble.svg"
+import instagramIcon from "./images/instagram.svg"
+
+export default {
+    //(Please Do Not Remove The comma(,) after every variable)
+    //Change The Website Template
+
+    //   Header Details ---------------------
+    name: "Leonardo",
+    headerTagline: [
+        //Line 1 For Header
+        "Building digital",
+        //Line 2 For Header
+        "products, brands,",
+        //Line 3 For Header
+        "and solutions",
+    ],
+    //   Header Paragraph
+    headerParagraph:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+
+    //Contact Email
+    contactEmail: "leoplazavzla@gmail.com",
+
+    // End Header Details -----------------------
+
+    // Work Section ------------------------
+    projects: [
+        {
+            title: "My first website", //Project Title - Add Your Project Title Here
+            para:
+                "This was my first website using HTML, CSS and JS. This is an affiliate website to make money with Amazon", // Add Your Service Type Here
+            //Project Image - Add Your Project Image Here
+            imageSrc:
+                "https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+            //Project URL - Add Your Project Url Here
+            github: "https://github.com/Leoplazavzla/tubatidoraideal",
+            url: "https://tubatidoraideal.com/",
+        },
+        {
+            title: "Productivity tools", //Project Title - Add Your Project Title Here
+            para:
+                "Web App made with ReactJS and Firebase. It's a simple CRUD application with authentication and storage", // Add Your Service Type Here
+            //Project Image - Add Your Project Image Here
+            imageSrc:
+                "https://images.unsplash.com/photo-1605153864431-a2795a1b2f95?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyNTY3ODl8fGVufDB8fHw%3D&auto=format&fit=crop&w=400&q=60",
+            //Project URL - Add Your Project Url Here
+            url: "https://leo-productivity-tools.web.app//",
+            github: "https://github.com/Leoplazavzla/leoProductivityTools"
+        },
+        {
+            title: "Alarm App", //Project Title - Add Your Project Title Here
+            para:
+                "This was my first Ecma Script application using OOP. It looks awful but it made me very happy :-)", // Add Your Service Type Here
+            //Project Image - Add Your Project Image Here
+            imageSrc:
+                "https://images.unsplash.com/photo-1504083898675-c896ecdae86e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+            //Project URL - Add Your Project Url Here
+            url: "https://leoplazavzla.github.io/alarm_app/",
+            github: "https://github.com/Leoplazavzla/alarm_app",
+        },
+    ],
+    pythonProjects: [
+        {
+            title: "Project Four", //Project Title - Add Your Project Title Here
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+            //Project Image - Add Your Project Image Here
+            imageSrc:
+                "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+            //Project URL - Add Your Project Url Here
+            url: "http://chetanverma.com/",
+        },
+        {
+            title: "Project Five", //Project Title - Add Your Project Title Here
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+            //Project Image - Add Your Project Image Here
+            imageSrc:
+                "https://images.unsplash.com/photo-1534239143101-1b1c627395c5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NzZ8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
+            //Project URL - Add Your Project Url Here
+            url: "http://chetanverma.com/",
+        },
+        {
+            title: "Project Six", //Project Title - Add Your Project Title Here
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.", // Add Your Service Type Here
+            //Project Image - Add Your Project Image Here
+            imageSrc:
+                "https://images.unsplash.com/photo-1517479149777-5f3b1511d5ad?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTAyfHxwYXN0ZWx8ZW58MHx8MHw%3D&auto=format&fit=crop&w=400&q=60",
+            //Project URL - Add Your Project Url Here
+            url: "http://chetanverma.com/",
+        },
+    ],
+
+    // End Work Section -----------------------
+
+    // About Secton --------------
+    aboutParaOne:
+        "I'm a digital marketing professional who decided to switch career into programming at my 30s. Coming from a destroyed nation and having to migrate to a new country, I am very familiar with the concept of start-over as I have started a new life.",
+    aboutParaTwo:
+        "I have experience not just with digital marketing but also Project Management. Being PRINCE2 and Agile Scrum my two major references in the subject. This makes me a good candidate as I am not only focused on the technical aspects of a project, but also the business and human side of it.",
+    aboutParaThree:
+        "I know how to communicate well, offer ideas and express any concerns. Also, being 33 and having worked the last 8 years for the same company, allows me to have a more focused mindset and a bigger sense of responsibility with the quality of my work.",
+    aboutImage:
+        "https://images.unsplash.com/photo-1519713958759-6254243c4a53?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+
+    //   End About Section ---------------------
+
+    // Skills Section ---------------
+
+    //   Import Icons from the top and link it here
+
+    skills: [
+        {
+            img: cssIcon,
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        },
+        {
+            img: jsIcon,
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        },
+        {
+            img: codeIcon,
+            para:
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,",
+        },
+    ],
+
+    // End Skills Section --------------------------
+
+    //   Promotion Section --------------------------
+
+    promotionHeading: "Heading",
+    promotionPara:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    // End Promotion Section -----------------
+
+    //   Contact Section --------------
+
+    contactSubHeading: "Let's create your next software solution together",
+    social: [
+        // Add Or Remove The Link Accordingly
+        { img: githubIcon, url: "https://github.com/Leoplazavzla" },
+        {
+            img: codepenIcon,
+            url: "https://www.linkedin.com/in/plazaleonardo/",
+        },
+        {
+            img: dribbbleIcon,
+            url: "https://dribbble.com/chetanverma",
+        },
+        {
+            img: instagramIcon,
+            url: "https://www.instagram.com/",
+        },
+    ],
+
+    // End Contact Section ---------------
+}
+
+// Thanks for using this template, I would love to hear from you contact me at hello@chetanverma.com
